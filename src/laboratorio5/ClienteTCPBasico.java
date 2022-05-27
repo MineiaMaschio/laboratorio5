31e1336badf246eb5f8af81b10561994a69c0834
SENIOR\mineia.maschio <mineia.maschio@senior.com.br>
2022-05-27 16:19:23 -0300

    teste tcp

---
+++ src/laboratorio5/ClienteTCPBasico.java
@@ -0,0 +1,39 @@
+package laboratorio5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClienteTCPBasico {
+	  public static void main(String[] args) {
+	    try {
+	      Socket cliente = new Socket("larc.inf.furb.br",1012);
+	      System.out.println(cliente);
+
+
+	      PrintStream out = new PrintStream( cliente.getOutputStream() );
+          BufferedReader in = new BufferedReader( new InputStreamReader( cliente.getInputStream() ) );
+	      
+          out.println( "GET " + " USERS 4268:ohfsp " + " HTTP/1.0" );
+          out.println();
+          
+          String line = in.readLine();
+          while( line != null )
+          {
+              System.out.println( line );
+              line = in.readLine();
+          }
+
+          // Close our streams
+          in.close();
+          out.close();
+          cliente.close();
+          
+	      System.out.println("Conexão encerrada");
+	    }
+	    catch(Exception e) {
+	      System.out.println("Erro: " + e.getMessage());
+	    }
+	  }
+	}
